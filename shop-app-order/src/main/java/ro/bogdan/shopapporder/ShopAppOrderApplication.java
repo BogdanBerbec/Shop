2073a0b03b93cceb84d8afac4ec98ba0a73b2425
BogdanBerbec <57673132+BogdanBerbec@users.noreply.github.com>
2020-08-24 19:12:33 +0300

    Add files via upload

---
+++ shop-app-order/src/main/java/ro/bogdan/shopapporder/ShopAppOrderApplication.java
@@ -0,0 +1,13 @@
+package ro.bogdan.shopapporder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopAppOrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopAppOrderApplication.class, args);
+	}
+
+}
---
+++ shop-app-order/src/test/java/ro/bogdan/shopapporder/ShopAppOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.bogdan.shopapporder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopAppOrderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
